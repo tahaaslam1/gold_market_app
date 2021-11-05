6c2d033283ab80b7575dff8c4d10b344a49ec798
Taha Aslam <k190147@nu.edu.pk>
2021-11-05 06:49:57 +0000

    drawer

--- production/gold_zoid/lib/views/widgets/drawer/drawer_navigation_widget.dart
+++ production/gold_zoid/lib/views/widgets/drawer/drawer_navigation_widget.dart
@@ -1,33 +1,37 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:gold_zoid/constants.dart';
 
 class DrawerNavigationWidget extends StatelessWidget {
-  
   final String text;
-  
+
   DrawerNavigationWidget({@required this.text});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left:30.0,right:170.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Icon(
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          flex: 2,
+          child: Icon(
             Icons.home,
             color: kActiveColor,
             size: 35.0,
           ),
-          Text(
+        ),
+        Expanded(
+          flex: 3,
+          child: Text(
             text,
             style: TextStyle(
               fontSize: 20.0,
               color: kInActiveColor,
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
