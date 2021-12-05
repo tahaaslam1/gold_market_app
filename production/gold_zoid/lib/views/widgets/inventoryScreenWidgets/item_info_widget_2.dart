1bfe14d1d0f08e9b407d48ab3c61dc6dd281f377
Taha Aslam <k190147@nu.edu.pk>
2021-12-05 17:10:16 +0500

    2nd bottom sheet

--- production/gold_zoid/lib/views/widgets/inventoryScreenWidgets/item_info_widget_2.dart
+++ production/gold_zoid/lib/views/widgets/inventoryScreenWidgets/item_info_widget_2.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gold_zoid/constants.dart';
+import 'package:gold_zoid/views/widgets/inventoryScreenWidgets/bottom_sheet_item_info_update.dart';
 
 class ItemInfoWidget2 extends StatelessWidget {
   @override
@@ -8,6 +9,7 @@ class ItemInfoWidget2 extends StatelessWidget {
       padding: const EdgeInsets.all(20.0),
       child: InkWell(
         onLongPress: () {
+          _showDeleteUpdateBottomSheet(context);
           print('item delete krne ki bottom sheet khul jae');
           // Navigator.pushNamed(context,'/inventoryScreen');
         },
@@ -128,4 +130,160 @@ class ItemInfoWidget2 extends StatelessWidget {
       ),
     );
   }
+}
+void _showDeleteUpdateBottomSheet(context) {
+  showModalBottomSheet(
+    shape: RoundedRectangleBorder(
+      borderRadius:BorderRadius.only(topLeft: Radius.circular(15.0),topRight: Radius.circular(15.0),), 
+    ),
+    isScrollControlled: true,
+    context: context,
+    builder: (BuildContext bc) {
+      return Wrap(children: [_deleteUpdateBottomSheetMenu()]);
+    },
+  );
+}
+Column _deleteUpdateBottomSheetMenu() {
+  return Column(
+      children: [
+        SizedBox(
+          height: 3.0,
+        ),
+        Divider(
+          height: 20.0,
+          color: kPrimaryTextColor,
+          thickness: 3.0,
+          endIndent: 160.0,
+          indent: 160.0,
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Text(
+                'Delete Gold Item',
+                style: TextStyle(
+                  color: kSecondaryTextColor,
+                  fontSize: 17.0,
+                ),
+              ),
+            ),
+             Padding(
+               padding: const EdgeInsets.only(right:15.0),
+               child: InkWell(
+                    onTap: () {
+                      //TODO: delete current selected item logic 
+                      // some logic...
+                    },
+                    child: Container(
+                      child: Center(
+                        child: Text(
+                          'Delete item',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0,
+                          ),
+                        ),
+                      ),
+                      height: 40.0,
+                      width: 120.0,
+                      decoration: BoxDecoration(
+                        
+                        color: Colors.red[400],
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(25.0),
+                        ),
+                      ),
+                    ),
+                  ),
+             ),
+          ],
+        ),
+        SizedBox(height: 20.0,),
+        Divider(
+          color: kPrimaryTextColor,
+          thickness: 1.0,
+        ),
+        SizedBox(height: 10.0,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Text(
+                'Update Gold Item',
+                style: TextStyle(
+                  color: kSecondaryTextColor,
+                  fontSize: 17.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 40.0,
+        ),
+        BottomSheetItemInfoUpdate(
+          text: 'Weight',
+          quantity: '2',
+          onMinus: () {
+            print('decrement by 5');
+          },
+          onPlus: () {
+            print('increment by 5');
+          },
+        ),
+        SizedBox(height: 30.0),
+        BottomSheetItemInfoUpdate(
+          text: 'Quantity',
+          quantity: '2',
+          onMinus: () {
+            print('decrement by 1');
+          },
+          onPlus: () {
+            print('increment by 1');
+          },
+        ),
+        SizedBox(height: 30.0),
+        BottomSheetItemInfoUpdate(
+          text: 'Karrot',
+          quantity: '2',
+          onMinus: () {
+            print('decrement from 24 to miinmum karrot jou huta h');
+          },
+          onPlus: () {
+            print('increment from min karrot jou huta h to 24k ');
+          },
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        Divider(
+          color: kPrimaryTextColor,
+          thickness: 1.0,
+        ),
+        SizedBox(height: 15.0),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 15.0),
+          child: Center(
+            child: InkWell(
+              onTap: () {
+                print("add the new item");
+              },
+              child: Text(
+                'Update item',
+                style: TextStyle(
+                  color: kPrimaryTextColor,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
 }
